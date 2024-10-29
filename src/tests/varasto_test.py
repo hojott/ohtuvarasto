ec520213b6dd4333aa63755e354cefd46c2c3fb6
Sakari Marttinen <sakkenino@gmail.com>
2024-10-29 12:19:43 +0200

    Up Varasto coverage to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -13,6 +13,18 @@ class TestVarasto(unittest.TestCase):
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
+    def test_konstruktori_luo_negatiivisen_saldon(self):
+        varasto = Varasto(10, -1)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_konstruktori_luo_negatiivisen_varaston(self):
+        varasto = Varasto(-1)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_konstruktori_luo_liian_pienen_tilavuuden(self):
+        varasto = Varasto(5, 10)
+        self.assertAlmostEqual(varasto.saldo, 5)
+
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +50,31 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_ottaminen_liikaa_tilaa(self):
+        self.varasto.ota_varastosta(7)
+
+        # varastossa pitäisi olla tilaa 4 + 7 = 11 -> 10
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_ottaminen_negatiivista_tilaa(self):
+        self.varasto.ota_varastosta(-1)
+
+        # varastossa pitäisi olla tilaa 10 + 0 = 10
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_laittaminen_liikaa_tilaa(self):
+        self.varasto.lisaa_varastoon(11)
+        
+        #varastossa pitäisi olla tilaa 10 - 11 = -1 -> 0
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_laittaminen_negatiivista_tilaa(self):
+        self.varasto.ota_varastosta(10)
+        self.varasto.lisaa_varastoon(-1)
+
+        #varastossa pitäisi olla tilaa 0 + 10 - 0 = 10
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test___str__(self):
+        self.assertEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
